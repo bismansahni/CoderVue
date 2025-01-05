fa52224eb69bba3942bf6fa4e98cf6595466b43d
bismansahni <bismansahni@outlook.com>
2025-01-05 22:59:53 +0530

    changed typo in dashboard.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
                     <h1 className="text-3xl font-bold text-gray-900">
-                        Convo<span className="text-blue-600">Vue</span>
+                        Coder<span className="text-blue-600">Vue</span>
                     </h1>
                     <UserButton/>
                 </div>
